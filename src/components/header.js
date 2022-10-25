8ac8a4b9fb734e2d5d60ffff98b2c3beeee77361
Kassian Wren <the@nodebotani.st>
2022-10-25 11:44:25 -0500

    Got header slice out

---
+++ src/components/header.js
@@ -0,0 +1,24 @@
+import * as React from "react"
+import { Link } from "gatsby"
+
+export function Header ({children, size = 'medium'}) {
+  let header = (
+    <Link className="header-link-home" to="/">
+      {children}
+    </Link>
+  )
+
+  if (size === "large") {
+    header = (
+      <h1 className="main-heading">
+        <Link to="/">{children}</Link>
+      </h1>
+    )
+  }
+
+  return (
+    <header className="global-header">
+      {header}
+    </header>
+  )
+}
